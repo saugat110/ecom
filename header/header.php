0ab345233ef50444cacc75aa6e5309a3238e81a7
saugat sthapit <saugatsthapit3@gmail.com>
2024-02-19 17:09:13 +0545

    initial_commit_1

---
+++ header/header.php
@@ -0,0 +1,27 @@
+
+<nav class="navbar navbar-expand-md bg-body-tertiary">
+  <div class="container-fluid">
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <img src="site_image/sitelogo.png" class="me-5" id="nav_logo">
+    <div class="collapse navbar-collapse" id="navbarNav">
+      <ul class="navbar-nav">
+        <li class="nav-item">
+          <a class="nav-link active me-4" aria-current="page" href="#">Home</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link me-4" href="#">AboutUs</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link me-4" href="#">Contact</a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" href="#">
+                <img src="site_image/cart.png" id="cart_image" class="pb-2">
+            </a>
+          </li>
+      </ul>
+    </div>
+  </div>
+</nav>
---
+++ index.php
@@ -0,0 +1,24 @@
+<?php
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    
+    <link rel="stylesheet" href="css/bs_css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/navbar.css">
+</head>
+<body>
+    <div class="container-fluid p-0">
+        <?php include_once('header/header.php'); ?>
+
+    </div>
+    
+
+    <script src="js/bs_js/bootstrap.min.js"></script>
+</body>
+</html>
